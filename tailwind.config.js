765fc01cd6be00cd3f112f70a534f5823618a8da
Bayram Çınar <bayram200212@gmail.com>
2024-02-20 13:43:46 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "jit", // JIT modunu etkinleştirin
+  purge: ["./index.html"], // Sadece index.html dosyasını purge et
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
